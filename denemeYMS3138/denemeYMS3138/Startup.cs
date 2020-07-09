fb20898f6455741fb7918cef0c84d43f460f229b
Rıdvan Aksoy <ridvanaksoy@hotmail.com.tr>
2020-07-09 11:12:52 +0300

    startup düzenlendi

---
+++ denemeYMS3138/denemeYMS3138/Startup.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace denemeYMS3138
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(x => x.EnableEndpointRouting = false);
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(x =>
+            {
+                x.MapRoute(
+    name: "default",
+    template: "{controller=Home}/{action=Index}/{id?}"
+
+    );
+            });
+        }
+    }
+}
